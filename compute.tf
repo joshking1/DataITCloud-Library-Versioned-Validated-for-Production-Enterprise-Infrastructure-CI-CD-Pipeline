b54a312b588aebdd169431ee22b9cd885fa0cfab
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-10-02 17:08:05 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -30,6 +30,22 @@ resource "aws_instance" "jenkins-instance" {
     Name = "Jenkins-Instance"
   }
 }
+resource "aws_instance" "jenkins-instance" {
+  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type   = "t2.medium"
+  key_name        = "${var.keyname}"
+  #vpc_id          = "${aws_vpc.development-vpc.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
+  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+  #name            = "${var.name}"
+  #user_data = "${file("Software-Applications-CI-CD.sh")}"
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "ansible"
+  }
+
+
 
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
