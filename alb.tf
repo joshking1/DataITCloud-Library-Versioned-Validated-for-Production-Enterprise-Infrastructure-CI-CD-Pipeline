88050d063d9bfe1f5fd79397b4108035e393c6cb
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-10-02 19:30:39 -0400

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -6,29 +6,24 @@ resource "aws_lb" "external-alb" {
   security_groups    = [aws_security_group.sg_allow_ssh_jenkins.id]
    
   subnet_mapping {
-    subnet_id     = "${aws_subnet.public-subnet-1.id}"
+    subnet_id     = aws_subnet.public-subnet-1.id
   }
 
   subnet_mapping {
-    subnet_id     = "${aws_subnet.public-subnet-2.id}"
+    subnet_id     = aws_subnet.public-subnet-2.id
   }
-
 }
 
 resource "aws_lb_target_group" "target-elb" {
   name     = "target-elb"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${aws_vpc.development-vpc.id}"
-}
-
-resource "aws_lb_target_group" "external-alb" {
-  # ...
+  vpc_id   = aws_vpc.development-vpc.id
 }
 
 resource "aws_lb_target_group_attachment" "attachment" {
-  target_group_arn = "${aws_lb_target_group.external-alb.arn}"
-  target_id        = "${aws_instance.jenkins-instance.id}"
+  target_group_arn = aws_lb_target_group.target-elb.arn
+  target_id        = aws_instance.jenkins-instance.id
   port             = 80
 
   depends_on = [
@@ -37,8 +32,8 @@ resource "aws_lb_target_group_attachment" "attachment" {
 }
 
 resource "aws_lb_target_group_attachment" "attachment1" {
-  target_group_arn = "${aws_lb_target_group.external-alb.arn}"
-  target_id        = "${aws_instance.jenkins-instance.id}"
+  target_group_arn = aws_lb_target_group.target-elb.arn
+  target_id        = aws_instance.jenkins-instance.id
   port             = 80
 
   depends_on = [
@@ -53,6 +48,6 @@ resource "aws_lb_listener" "external-elb" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.external-alb.arn
+    target_group_arn = aws_lb_target_group.target-elb.arn
   }
 }
