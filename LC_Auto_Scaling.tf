0ab6469b6acee6548df40a22f778ad462b5c075b
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-10-02 19:35:02 -0400

    Update LC_Auto_Scaling.tf

--- LC_Auto_Scaling.tf
+++ LC_Auto_Scaling.tf
@@ -1,7 +1,10 @@
-resource "aws_launch_configuration" "as_conf" {
-  name_prefix   = "terraform-lc-example-"
-  image_id      = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type = "t2.micro"
+resource "aws_autoscaling_group" "as_conf" {
+  name                 = "new-terraform-asg-example"
+  availability_zones  = ["us-east-2a"]
+  health_check_type   = "EC2"
+  launch_configuration = aws_launch_configuration.as_conf.name
+  min_size             = 1
+  max_size             = 2
 
   lifecycle {
     create_before_destroy = true
