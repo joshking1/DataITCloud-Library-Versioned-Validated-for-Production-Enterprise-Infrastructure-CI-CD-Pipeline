7cc621de97c92f1ff58623df08f4e5cd318457ac
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-04 22:55:27 -0400

    Update ubuntu.tf

--- ubuntu.tf
+++ ubuntu.tf
@@ -14,3 +14,15 @@ data "aws_ami" "ubuntu_server" {
   owners = ["099720109477"]  # Canonical owner ID
 }
 
+# Creating ubuntu server 
+
+resource "aws_instance" "ubuntu-server" {
+  ami           = data.aws_ami.ubuntu_server.id
+  instance_type = "t2.medium"  # Replace with your desired instance type
+  key_name      = "your-key-pair"  # Replace with your key pair name
+
+  tags = {
+    Name = "Ubuntu-22.04-Instance"
+  }
+}
+
