563074d885342dda711214022c576d6b9f4424bb
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-28 22:18:46 -0400

    Update Software-Applications-CI-CD.sh

--- Software-Applications-CI-CD.sh
+++ Software-Applications-CI-CD.sh
@@ -1,62 +1,56 @@
 #!/bin/bash
-sudo yum update 
 
-sudo yum upgrade -y
+# Update and upgrade the system
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+# Install Apache, MariaDB, and PHP
+sudo apt-get install -y apache2 mariadb-server php php-mysql
 
-sudo cat /etc/system-release
+# Start and enable Apache
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
-sudo yum install -y httpd mariadb-server
+# Install Java JDK 11
+sudo apt-get install -y openjdk-11-jdk
 
-sudo systemctl start httpd
+# Install Maven
+sudo apt-get install -y maven
 
-sudo systemctl enable httpd
+# Install Git
+sudo apt-get install -y git
 
-sudo systemctl is-enabled httpd
+# Install Ansible
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update -y
+sudo apt-get install -y ansible
 
-echo "Install Java JDK 11"
-sudo yum remove -y java
-sudo amazon-linux-extras install java-openjdk11
+# Install Nginx
+sudo apt-get install -y nginx
 
-echo "Install Maven" 
+# Install Python pip
+sudo apt-get install -y python3-pip
 
-echo "Install git"
-yum install git -y
+# Install AWS CLI
+sudo apt-get install -y awscli
 
-amazon-linux-extras install epel
-yum update -y
-yum install ansible -y
-yum install nginx -y
-yum install python-pip -y  
-yum install awscli 
-yum install boto -y 
-cd /home 
-touch hug.yml 
-touch nat.yml 
+# Install Boto
+pip3 install boto
 
-echo " Install Terraform"
-sudo yum install update -y 
-sudo wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip 
-sudo unzip terraform_1.1.2_linux_amd64.zip
-sudo mv terraform /usr/bin/
-terraform --version
+# Install Terraform
+sudo apt-get install -y wget unzip
+wget https://releases.hashicorp.com/terraform/1.1.2/terraform_1.1.2_linux_amd64.zip 
+unzip terraform_1.1.2_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
 
-echo "Install Docker engine"
-yum update -y
-yum install docker -y
-systemctl start docker
-systemctl enable docker
-systemctl status docker
-#sudo usermod -a -G docker jenkins
-#sudo service docker start
-sudo chkconfig docker on
+# Install Docker engine
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
 
-echo " install Alpine"
-
-
-echo "Install Jenkins"
-docker run \
+# Install Jenkins
+sudo docker run \
   -u root \
   --rm \
   -d \
@@ -67,22 +61,4 @@ docker run \
   -v $(which docker):/usr/bin/docker \
   -v /home/jenkins_home:/var/jenkins_home \
   jenkins/jenkins
-  
-  
-docker exec -it myjenkin bash
-cat /var/jenkins_home/secrets/initialAdminPassword
-exit
-docker logs myjenkin
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-yum install jenkins -y
-amazon-linux-extras install epel -y
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-sudo systemctl status jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
-java -jar jenkins.war\
-
-
 
