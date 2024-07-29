aa42ee9313de3f4344ba4c02f7a746f627f4187b
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-28 22:08:10 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,30 +1,10 @@
-# Amazon Linux 2 Server
-
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = [
-      "amzn2-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-  filter {
-    name = "owner-alias"
-    values = [
-      "amazon",
-    ]
-  }
-}
-
-# ubuntu server 
+# Ubuntu Server AMI
 
 data "aws_ami" "ubuntu_server" {
   most_recent = true
 
   filter {
     name = "name"
-    #values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
@@ -36,81 +16,51 @@ data "aws_ami" "ubuntu_server" {
   owners = ["099720109477"] # Canonical
 }
 
-# Ubuntu Java Application Server 
-
-resource "aws_instance" "ubuntu-server" {
-  ami           = "${data.aws_ami.ubuntu_server.id}"
-  instance_type = "t2.medium"  
-  key_name      = "${var.keyname}"  
-  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  associate_public_ip_address = true
-  tags = {
-    Name = "Ubuntu-22.04-Instance"
-  }
-}
+# Jenkins Instance
 
 resource "aws_instance" "jenkins-instance" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  ami             = "${data.aws_ami.ubuntu_server.id}"
   instance_type   = "t2.small"
   key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
   user_data = "${file("Software-Applications-CI-CD.sh")}"
 
   associate_public_ip_address = true
   tags = {
-    Name = "jenkins-Instance"
+    Name = "jenkins-instance"
   }
 }
+
+# Ansible Instance
+
 resource "aws_instance" "ansible-instance" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  ami             = "${data.aws_ami.ubuntu_server.id}"
   instance_type   = "t2.medium"
   key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
-  #user_data = "${file("Software-Applications-CI-CD.sh")}"
-
   associate_public_ip_address = true
   tags = {
     Name = "ansible-instance"
   }
 }
+
+# Jenkins Agent 1
+
 resource "aws_instance" "jenkins-agent-1" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  ami             = "${data.aws_ami.ubuntu_server.id}"
   instance_type   = "t2.medium"
   key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
   vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
   subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
-  #user_data = "${file("Software-Applications-CI-CD.sh")}"
-
   associate_public_ip_address = true
   tags = {
     Name = "jenkins-agent-1"
   }
 }
 
-resource "aws_instance" "jenkins-agent-2" {
-  ami             = "${data.aws_ami.amazon-linux-2.id}"
-  instance_type   = "t2.medium"
-  key_name        = "${var.keyname}"
-  #vpc_id          = "${aws_vpc.development-vpc.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
-  subnet_id          = "${aws_subnet.public-subnet-1.id}"
-  #name            = "${var.name}"
-  #user_data = "${file("Software-Applications-CI-CD.sh")}"
-
-  associate_public_ip_address = true
-  tags = {
-    Name = "jenkins-agent-2"
-  }
-}
+# Security Group for SSH and Jenkins
 
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
@@ -123,7 +73,8 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
