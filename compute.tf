767808108157aac42664b1bac951133ae4bdf03f
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-30 17:01:56 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -46,7 +46,50 @@ resource "aws_instance" "jenkins-instance" {
   }
 }
 
+# SonarQube Instance
+
+resource "aws_instance" "sonarqube-instance" {
+  ami             = "${data.aws_ami.ubuntu_server.id}"
+  instance_type   = "t2.medium"
+  key_name        = "${var.keyname}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
+  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+  associate_public_ip_address = true
+  tags = {
+    Name = "sonarqube-instance"
+  }
+}
+
+# Prometheus Instance
+
+resource "aws_instance" "prometheus-instance" {
+  ami             = "${data.aws_ami.ubuntu_server.id}"
+  instance_type   = "t2.medium"
+  key_name        = "${var.keyname}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
+  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+  associate_public_ip_address = true
+  tags = {
+    Name = "prometheus-instance"
+  }
+}
+
+# Grafana Instance
+
+resource "aws_instance" "grafana-instance" {
+  ami             = "${data.aws_ami.ubuntu_server.id}"
+  instance_type   = "t2.medium"
+  key_name        = "${var.keyname}"
+  vpc_security_group_ids = ["${aws_security_group.sg_allow_ssh_jenkins.id}"]
+  subnet_id          = "${aws_subnet.public-subnet-1.id}"
+  associate_public_ip_address = true
+  tags = {
+    Name = "grafana-instance"
+  }
+}
+
 # Jenkins-Agent-1
+
 resource "aws_instance" "jenkins-agent-1" {
   ami             = "${data.aws_ami.ubuntu_server.id}"
   instance_type   = "t2.medium"
@@ -59,11 +102,11 @@ resource "aws_instance" "jenkins-agent-1" {
   }
 }
 
-# Security Group for SSH and Jenkins
+# Security Group for SSH, Jenkins, SonarQube, Prometheus, and Grafana
 
 resource "aws_security_group" "sg_allow_ssh_jenkins" {
   name        = "allow_ssh_jenkins"
-  description = "Allow SSH and Jenkins inbound traffic"
+  description = "Allow SSH, Jenkins, SonarQube, Prometheus, and Grafana inbound traffic"
   vpc_id      = "${aws_vpc.development-vpc.id}"
 
   ingress {
@@ -93,6 +136,34 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 9092
+    to_port     = 9092
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port       = 0
@@ -101,3 +172,4 @@ resource "aws_security_group" "sg_allow_ssh_jenkins" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
