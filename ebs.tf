d029a748ac82ba3ddfb5d9e40af7e4c55b42770f
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-08-01 23:14:04 -0400

    Update ebs.tf

--- ebs.tf
+++ ebs.tf
@@ -6,5 +6,5 @@ resource "aws_ebs_volume" "jenkins_instance" {
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.jenkins_instance.id
-  instance_id = aws_spot_instance_request.jenkins_instance.id
+  instance_id = aws_spot_instance_request.jenkins_instance.spot_instance_id
 }
