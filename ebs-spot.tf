e13c37f0adedf4b5c9c2236d774b641266e063a3
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-08-03 07:45:19 -0400

    Update and rename ebs to ebs-spot.tf

--- ebs-spot.tf
+++ ebs-spot.tf
@@ -1,10 +1,13 @@
+# Define the EBS volume
 resource "aws_ebs_volume" "jenkins_instance" {
   availability_zone = "${var.region}a"
   size              = 100
 }
 
+# Attach the EBS volume to the Spot instance
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.jenkins_instance.id
-  instance_id = aws_spot_instance_request.jenkins_instance.spot_instance_id
+  instance_id = aws_spot_instance_request.jenkins-instance.spot_instance_id
 }
+
