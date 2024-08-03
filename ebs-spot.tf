d1bfc9c3f8ce60149ad4f98349180df3e05e64bc
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-08-03 08:25:46 -0400

    Update ebs-spot.tf

--- ebs-spot.tf
+++ ebs-spot.tf
@@ -7,7 +7,7 @@ resource "aws_ebs_volume" "jenkins_instance" {
 # Attach the EBS volume to the Spot instance
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.jenkins_instance.id
-  instance_id = aws_spot_instance_request.jenkins-instance.spot_instance_id
+  volume_id   = aws_ebs_volume.jenkins_instance.id   
+  instance_id = aws_spot_instance_request.ansible-instance.id
 }
 
